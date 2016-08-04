ea36ee638b6168459414bf8250e61676e4565660
Daniel Lim <hbt@markhamboards.com>
2016-08-04 14:29:28 -0400

    add routes
    add routes

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -17,10 +17,14 @@ export class Notes {
   constructor(private noteService:NoteService) {
     this.noteService.getNotes().subscribe(res => this.notes = res.data);
   }
-  
-  onNoteChecked(note, i) {
-    this.notes.splice(i, 1);
-  };
+
+  onNoteChecked(note) {
+    this.noteService.completeNote(note)
+    .subscribe(note => {
+      const i = this.notes.findIndex(localNote => localNote.id === note.id);
+        this.notes.splice(i, 1);
+    });
+  }
 
   onCreateNote(note) {
     this.noteService.createNote(note).subscribe(note => this.notes.push(note));
@@ -29,4 +33,5 @@ export class Notes {
   ngOnDestroy() {
     console.log('destroyed');
   }
+
 }
