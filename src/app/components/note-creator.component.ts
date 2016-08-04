861e055a6095625fba1d350113c68b879ac7806e
Daniel Lim <hbt@markhamboards.com>
2016-08-04 00:04:18 -0400

    edit out previous set of colors
    ['#320E3B', '#7F96FF', '#E56399', '#A6CFD5', '#71A9F7', '#DBFCFF',
    '#6B5CA5', '#C6D8FF', '#72195A', '#4C1036'];

--- src/app/components/note-creator.component.ts
+++ src/app/components/note-creator.component.ts
@@ -11,7 +11,7 @@ import { ColorPickr } from '../ui/color-picker';
 export class NoteCreator {
     @Output() createNote = new EventEmitter();
 
-    colors: Array<string> = ['#320E3B', '#7F96FF', '#E56399', '#A6CFD5', '#71A9F7', '#DBFCFF', '#6B5CA5', '#C6D8FF', '#72195A', '#4C1036'];
+    colors: Array<string> = []; /* ['#320E3B', '#7F96FF', '#E56399', '#A6CFD5', '#71A9F7', '#DBFCFF', '#6B5CA5', '#C6D8FF', '#72195A', '#4C1036']; */
 
     newNote = {
       title: '',
