cdcf76abb7d1ae0b18e275ec23db1037996da488
Daniel Lim <hbt@markhamboards.com>
2016-08-04 14:50:54 -0400

    change nav bar
    change nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Main } from './containers';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component ({
   moduleId: module.id,
   selector: 'app',
-  directives: [Main],
+  directives: [Main, ...ROUTER_DIRECTIVES],
   providers: [HTTP_PROVIDERS],
   templateUrl: './app/app.component.html'
 })
--- src/app/ui/nav-bar.ts
+++ src/app/ui/nav-bar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component ({
   moduleId: module.id,
   selector: 'navi-bar',
+  directives: [...ROUTER_DIRECTIVES],
   styleUrls: ['app/ui/style.ui.css'],
   templateUrl: 'app/ui/navibar.html'
 })
