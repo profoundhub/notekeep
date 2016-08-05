638845b0a71525f7ac7507ee717938bddf1c0e7f
Daniel Lim <hbt@markhamboards.com>
2016-08-04 21:01:10 -0400

    Test Auth
    Test Auth

--- src/app/containers/auth.ts
+++ src/app/containers/auth.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FORM_DIRECTIVES } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'auth-container',
+  directives: [...FORM_DIRECTIVES],
   styleUrls: ['./app/containers/auth.css'],
   templateUrl: './app/containers/auth.html'
 })
 
-export class Auth {}
+export class Auth {
+  user = {
+    password: '',
+    email: ''
+  };
+
+  mode: string = 'sign-in';
+  linkText: string = 'Don\'t have an account?';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  changeMode() {
+    if (this.mode === 'sign-in') {
+      this.mode = 'sign-up'
+      this.linkText = 'Already have an Account?'
+    } else {
+      this.mode = 'sign-in';
+      this.linkText = 'Don\'t have an Account?';
+    }
+  }
+
+  authenticate() {
+    this.auth.authenticate(this.mode, this.user)
+    .subscribe(() => this.router.navigate(['']))
+  }
+
+}
