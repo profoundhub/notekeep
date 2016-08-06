4a1a728c59917b8045fdcf6af40f722fb5cbc8aa
Daniel Lim <hbt@markhamboards.com>
2016-08-05 21:24:08 -0400

    update changes
    update changes

--- spec.entry.ts
+++ spec.entry.ts
@@ -16,9 +16,6 @@ import 'rxjs/add/observable/throw';
 import { setBaseTestProviders } from '@angular/core/testing';
 import { TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS, TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from '@angular/platform-browser-dynamic/testing';
 
-import './src/app/services/api.spec';
-import './src/app/ui/note-card.spec';
-
 setBaseTestProviders(
   TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
   TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
